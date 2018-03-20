d0d24c952027de449b220a7b98f92373734b74a7
Tomasz Batko <batkot@protonmail.com>
2018-03-20 22:07:48 +0100

    Add Sass Webpack integration

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 'use strict'
 
+require("./static/style.scss");
+
 var f = x => x + 1;
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -6,12 +6,25 @@ const indexPath = "src/static/index.html";
 const path = require('path');
 const cleanWebpackPlugin = require('clean-webpack-plugin');
 const htmlWebpackPlugin = require('html-webpack-plugin');
+const extractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const extractSass = new extractTextWebpackPlugin({
+    filename: '[name]-[contenthash].css'
+});
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.join(__dirname, distFolder),
         filename: fileName
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            exclude: [/elm-stuff/, /node_modules/],
+            use: extractSass.extract({
+                use: [ "css-loader", "sass-loader"]
+            })
+        }]
     },
     plugins: [
         new cleanWebpackPlugin([distFolder],{
@@ -20,6 +33,7 @@ module.exports = {
             verbose: true,
             dry: false
         }),
+        extractSass,
         new htmlWebpackPlugin({
             template: indexPath,
             inject: "body",
