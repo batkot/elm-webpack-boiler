01f4f3e2ea7eb88ebe8671f60cd24bedc8d306a0
Tomasz Batko <batkot@protonmail.com>
2019-09-20 22:59:23 +0200

    Add cleanup before build
    - Using clean-webpack-plugin

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -4,14 +4,20 @@ const path = require('path');
 // Plugin for index.html generation
 // Wires up scripts and stylesheets
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// Plugin for cleaning dist directory before build
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'production',
     entry: appConfig.entryPoint,
     output: {
         path: path.join(__dirname, appConfig.buildPath),
         filename: appConfig.appName
     },
     plugins: [
+        new CleanWebpackPlugin({
+            verbose: true
+        }),
         new HtmlWebpackPlugin()
     ]
 }
